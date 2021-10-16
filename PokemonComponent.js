2410f34926961e963283b06a85914108bc5ca966
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-16 17:46:04 +0200

    Added PokemonComponent class

--- PokemonComponent.js
+++ PokemonComponent.js
@@ -2,29 +2,23 @@ import Component from "./Component.js";
 class PokemonComponent extends Component{
 pokemon;
 
-  constructor (parentElement, className, tagName, pokemon) {
-    super(parentElement, className, tagName);
-    //this.element = document.createElement("li"); 
-    //this.element.className = "pokemon-item";
-    //this.parentElement.appendChild(this.element);
+  constructor (parentElement, pokemon) {
+    super(parentElement, "pokemon-item", "li");
     this.pokemon = pokemon;
     this.generateHTML();
-
   }
 
   generateHTML () {
     const html = `
-      <div class="name">${this.pokemon.name}</div>
-      <img src=${this.pokemon.sprites.front_default}/>
-      <div class="details">
-        <span>Height: ${this.pokemon.height} </span>
-        <span>Weight: ${this.pokemon.weight} </span>
-      </div>
+    <div class="pokemon">
+      <h2 class="pokemon_name">${this.pokemon.name}</h2>
+      <p class="pokemon_type">${this.pokemon.type}</p>
+      <p class="pokemon_id">Nº ${this.pokemon.id}<p>
+      <img src=${this.pokemon.img} alt ="${this.pokemon.name}Image" width=150 height=150 />
+    </div>  
     `;
     this.element.innerHTML = html;
   }
-
-
 }
 
 export default PokemonComponent;
