059d959a1644fe9493f52f43d6c8867608f8f475
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-16 14:16:59 +0200

    It prints all data for an individual pokemon

---
+++ Page.js
@@ -0,0 +1,19 @@
+import Component from "./Component.js";
+
+class Page extends Component {
+pokemons
+
+constructor(parentElement, className, tagName, pokemons) {
+  super(parentElement, className, tagName);
+  this.pokemons = pokemons;
+  this.generateHTML();
+
+
+
+}
+
+generateHTML () {
+
+}
+
+}
--- PokemonComponent.js
+++ PokemonComponent.js
@@ -1,17 +1,24 @@
+import Component from "./Component.js"; 
 class PokemonComponent extends Component{
+pokemon;
 
-  constructor () {
-    super(parentElement, className, tagName, url);
+  constructor (parentElement, className, tagName, pokemon) {
+    super(parentElement, className, tagName);
+    //this.element = document.createElement("li"); 
+    //this.element.className = "pokemon-item";
+    //this.parentElement.appendChild(this.element);
+    this.pokemon = pokemon;
+    this.generateHTML();
 
   }
 
   generateHTML () {
     const html = `
-      <div class="name">${this.name}</div>
-      <img src=${this.sprites.front_default}/>
+      <div class="name">${this.pokemon.name}</div>
+      <img src=${this.pokemon.sprites.front_default}/>
       <div class="details">
-        <span>Height: ${this.height} </span>
-        <span>Weight: ${this.weight} </span>
+        <span>Height: ${this.pokemon.height} </span>
+        <span>Weight: ${this.pokemon.weight} </span>
       </div>
     `;
     this.element.innerHTML = html;
