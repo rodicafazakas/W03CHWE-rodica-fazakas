2389d00ea25bc5f3ec8c451d65c8093f9a5158f9
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-17 10:23:29 +0200

    Fix pagination button

--- Button.js
+++ Button.js
@@ -7,8 +7,8 @@ class Button extends Component {
   constructor(parentElement, text, actionOnClick) {
     super(parentElement, "pagination", "button");
     this.text = text;
-    this.element.innerText = this.element;
-    this.element.actionOnClick = actionOnClick;
+    this.element.innerText = this.text;
+    this.actionOnClick = actionOnClick;
     this.element.addEventListener("click", this.actionOnClick);
 
   };
